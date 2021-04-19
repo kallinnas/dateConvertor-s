050dcf0c33e9111f39e30c3debbc6fb8163d2f99
Serhii Kalinichenko <kallinnas@gmail.com>
2021-04-19 12:15:31 +0300

    add HebrewDate model

---
+++ src/main/java/com/herokuapp/convert/gregtohebrew/model/HebrewDate.java
@@ -0,0 +1,12 @@
+package com.herokuapp.convert.gregtohebrew.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HebrewDate {
+    String date;
+}
