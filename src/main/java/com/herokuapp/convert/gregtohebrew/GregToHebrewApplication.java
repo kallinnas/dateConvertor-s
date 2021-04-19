cd823243a5c7cae4d7ca7a56088fda70ba4cba5e
Serhii Kalinichenko <kallinnas@gmail.com>
2021-04-19 12:12:25 +0300

    add spring app dep

---
+++ src/main/java/com/herokuapp/convert/gregtohebrew/GregToHebrewApplication.java
@@ -0,0 +1,14 @@
+package com.herokuapp.convert.gregtohebrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GregToHebrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GregToHebrewApplication.class, args);
+		System.out.println("Everything is fine!");
+	}
+
+}
---
+++ src/test/java/com/herokuapp/convert/gregtohebrew/GregToHebrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.convert.gregtohebrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GregToHebrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
